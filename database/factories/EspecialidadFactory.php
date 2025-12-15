b18fe01b7ca1cc4f5983176fd71ef80e51ee215f
jrhernandez1991 <jrhernandez1@espe.edu.ec>
2025-12-15 16:04:26 -0500

    Update EspecialidadFactory.php

--- database/factories/EspecialidadFactory.php
+++ database/factories/EspecialidadFactory.php
@@ -3,12 +3,14 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Especialidad;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Especialidad>
  */
 class EspecialidadFactory extends Factory
 {
+    protected $model = Especialidad::class;
     /**
      * Define the model's default state.
      *
@@ -17,7 +19,23 @@ class EspecialidadFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'nombre' => this->faker->unique()->randomElement([
+                'Cardiologia',
+                'Pediatria',
+                'Dermatologia',
+                'Neurologia',
+                'Gastroenterologia',
+                'Oftalmologia',                                             
+                'Ortopedia',
+                'Ginecologia',
+                'Urologia',
+                'Medicina General',
+                'Psiquiatria',                                            
+                'Endocrinologia',
+                'Neumologia',
+                'Oncologia',
+                'Reumatologia',
+            ])                                                 
         ];
     }
 }
