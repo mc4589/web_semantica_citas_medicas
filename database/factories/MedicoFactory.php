283626dfadd5529b0083ebdc04c5d4ec5b3c5fae
jrhernandez1991 <jrhernandez1@espe.edu.ec>
2025-12-15 17:27:33 -0500

    Update MedicoFactory.php

--- database/factories/MedicoFactory.php
+++ database/factories/MedicoFactory.php
@@ -3,12 +3,14 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-
+use App\Models\Medico;
+use App\Models Especialidad;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Medico>
  */
 class MedicoFactory extends Factory
 {
+    protected $model = Medico::class;
     /**
      * Define the model's default state.
      *
@@ -17,6 +19,9 @@ class MedicoFactory extends Factory
     public function definition(): array
     {
         return [
+            'nombre' => $this->faker->firstName() . ' ' . $this->faker->lastName(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'especialidad_id' => Especialidad::inRandomOrder()->first() ?? Especialidad::factory(),
             //
         ];
     }
