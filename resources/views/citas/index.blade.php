176c724e842b83128f6cdc9e8d0650352062313e
Eduardo Cantuña <118578087+EduardoCantunaCela@users.noreply.github.com>
2025-12-15 18:43:27 -0500

    Update index.blade.php

--- resources/views/citas/index.blade.php
+++ resources/views/citas/index.blade.php
@@ -1,3 +1,6 @@
 @extends('layout.app')
 
 @section('title', 'Citas Médicas')
+
+@section('header-title', 'Web Semántica - Citas Médicas')
+@section('header-subtitle', 'Listado interactivo consumiendo la API REST con JSON-LD')
