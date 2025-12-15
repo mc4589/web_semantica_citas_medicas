ec52d700fabfb3a239b25b7470d7319231c3c8d1
Eduardo Cantuña <118578087+EduardoCantunaCela@users.noreply.github.com>
2025-12-15 18:52:10 -0500

    Update index.blade.php

--- resources/views/especialidades/index.blade.php
+++ resources/views/especialidades/index.blade.php
@@ -7,3 +7,19 @@
 
 @section('content')
     <h3 style="color: #28a745; margin-top: 0;">Listado de Especialidades (cargado desde /api/v1/especialidades)</h3>
+
+    <table id="tabla-especialidades" class="table-api">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nombre</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr class="loading">
+                    <td colspan="3">Cargando datos desde la API...</td>
+                </tr>
+            </tbody>
+    </table>
+
