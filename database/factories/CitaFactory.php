321cb7ea65832e97213152fa3086314047fbbbdd
jrhernandez1991 <jrhernandez1@espe.edu.ec>
2025-12-15 15:30:25 -0500

    Update CitaFactory.php

--- database/factories/CitaFactory.php
+++ database/factories/CitaFactory.php
@@ -3,21 +3,24 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Cita;
+use App\Models\Medico;
+use App\Models\Paciente;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Cita>
- */
 class CitaFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition(): array
+   protected $model = Cita::class;
+    
+   public function definition(): array
     {
         return [
-            //
+            'fecha' => $this->faker->dateTimeBetween('+1 day', '+1 year')->format('Y-m-d'),
+            'hora' => $this->faker->time('H:i:s'),
+            'descripcion' => $this->faker->optional(0.9)->sentence(), //90% tienen descripcion
+            
+            //Crea o elige medico y pacientes existentes
+            'medico_id' => Medico::inRandomOrder()->first() ?? Medico::factory(),
+            'paciente_id' => Paciente::inRandomOrder()->first() ?? Paciente::factory(),
         ];
     }
 }
