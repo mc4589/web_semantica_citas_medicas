3a156c2a30c8e06d0da7a3982bd1c5af34e3d9a7
Eduardo Cantuña <118578087+EduardoCantunaCela@users.noreply.github.com>
2025-12-15 18:47:21 -0500

    Update show.blade.php

--- resources/views/citas/show.blade.php
+++ resources/views/citas/show.blade.php
@@ -1,3 +1,7 @@
 @extends('layout.app')
 
 @section('title', 'Detalle de Cita Médica')
+
+@section('header-title', 'Web Semántica - Detalle de Cita')
+@section('header-subtitle', 'Datos y JSON-LD cargados dinámicamente desde la API')
+
