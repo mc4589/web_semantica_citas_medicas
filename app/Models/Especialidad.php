17c278fc54ffa74892b8833766ffcbab043a5613
mc4589 <marcochacony@hotmail.com>
2025-12-15 00:08:23 -0500

    Update Especialidad.php

--- app/Models/Especialidad.php
+++ app/Models/Especialidad.php
@@ -2,14 +2,39 @@
 
 namespace App\Models;
 
+use App\Traits\HasJsonLd;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Especialidad extends Model
 {
-    
-    use HasFactory;
-    protected $table = 'especialidades';
+    use HasFactory, HasJsonLd;
 
     protected $fillable = ['nombre'];
+    
+    // Define las relaciones que deben cargarse si este modelo fuera la entidad principal en una respuesta
+    protected array $jsonLdRelations = ['medicos'];
+
+    /**
+     * Relación: Una especialidad tiene muchos médicos.
+     */
+    public function medicos(): HasMany
+    {
+        return $this->hasMany(Medico::class);
+    }
+
+    /**
+     * Mapeo a JSON-LD (MedicalSpecialty)
+     */
+    public function toJsonLd(): array
+    {
+        return [
+            '@context' => 'http://schema.org',
+            '@type' => 'MedicalSpecialty',
+            'identifier' => $this->id,
+            'name' => $this->nombre,
+           
+        ];
+    }
 }
