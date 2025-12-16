62be7b84468b46a1630221e566d14e62cc97da1b
Eduardo Cantuña <118578087+EduardoCantunaCela@users.noreply.github.com>
2025-12-15 19:00:08 -0500

    Update show.blade.php

--- resources/views/medicos/show.blade.php
+++ resources/views/medicos/show.blade.php
@@ -1,3 +1,6 @@
 @extends('layout.app')
 
 @section('title', 'Detalle de Médico')
+
+@section('header-title', 'Web Semántica - Detalle de Médico')
+@section('header-subtitle', 'Datos y JSON-LD cargados dinámicamente desde la API')
