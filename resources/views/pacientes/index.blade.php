594fea5d31377906d2e0a627b9b4a0fb06dcb61d
Eduardo Cantuña <118578087+EduardoCantunaCela@users.noreply.github.com>
2025-12-15 19:02:57 -0500

    Update index.blade.php

--- resources/views/pacientes/index.blade.php
+++ resources/views/pacientes/index.blade.php
@@ -1,3 +1,6 @@
 @extends('layout.app')
 
 @section('title', 'Pacientes')
+
+@section('header-title', 'Web Semántica - Pacientes')
+@section('header-subtitle', 'Listado interactivo consumiendo la API REST con JSON-LD')
